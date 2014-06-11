d3040cc8ee7e9ecee91ad649ef6b68fa351f68f6
Parris Khachi <parris@eventbrite.com>
2014-06-10 19:42:28 -0700

    Provide an example for data attributes

--- examples/lorem_plugin.js
+++ examples/lorem_plugin.js
@@ -1,3 +1,9 @@
 Dorsal.registerPlugin('lorem', function(options) {
-    options.el.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, eaque, sapiente, ea, porro soluta sed temporibus alias dicta iste quam similique unde consectetur rerum. Voluptas, quae soluta tempore illo ex.';
+    var type = options.data.nonStandardLorem || 'lorem',
+        loremMap = {
+            bacon: 'Bacon ipsum dolor sit amet jerky pastrami pork belly, meatloaf ground round kielbasa corned beef cow sausage. Pork belly leberkas boudin frankfurter. Capicola brisket prosciutto meatloaf pork. Pork belly landjaeger pancetta spare ribs shankle shoulder. Rump prosciutto jowl cow. Shankle chuck andouille t-bone rump.',
+            lorem: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, eaque, sapiente, ea, porro soluta sed temporibus alias dicta iste quam similique unde consectetur rerum. Voluptas, quae soluta tempore illo ex.'
+        };
+
+    options.el.innerHTML = loremMap[type];
 });
