8b5755ce88e60fcd17748e3d413e4234379216c8
gagoar <xeroice@gmail.com>
2014-07-15 17:09:43 -0300

    wrapping task with registerTask from Grunt

--- tasks/pushsite.js
+++ tasks/pushsite.js
@@ -1,15 +1,16 @@
-#!/usr/bin/env node
+module.exports = function (grunt) {
 
-var fs = require('fs'),
-    sh = require("shelljs"),
-    git = require('gift'),
-    opts = require('minimist'),
-    argv = opts(process.argv.slice(2)),
-    repo = git(sh.pwd()),
-    remote = argv.remote || 'origin',
-    branch = argv.branch || 'master',
-    ghPages = remote + '/gh-pages',
-    seed = Math.random().toString(36).substring(10).substring(0, 6);
+  var git = require('gift'),
+      cp = grunt.file.cp,
+      conf = grunt.config('push-site') || {},
+      repo = git(process.cwd()),
+      remote = grunt.option('remote') || conf.remote || 'origin',
+      branch = grunt.option('branch') || conf.branch || 'master',
+      message = grunt.option('message') || conf.message || 'updated dorsal site',
+      ghPages = remote + '/gh-pages',
+      seed = Math.random().toString(36).substring(10).substring(0, 6);
+
+  grunt.registerTask('push-site', function() {
 
     repo.remote_fetch(remote, function (e) {
 
@@ -23,17 +24,17 @@ var fs = require('fs'),
 
           if (e) { return new Error(e.text); }
 
-          sh.cp('-f', 'README.md', '_includes/');
+          cp('README.md', '_includes/README.md', {force: true});
 
           repo.add('_includes/README.md', function (e) {
 
             if (e) { return new Error(e.text); }
 
-            repo.commit('updated dorsal site', function (e) {
+            repo.commit(message, function (e) {
 
               repo.remote_push(remote, seed + ':gh-pages' , function (e) {
                 if (e) {
-                  return new Error(e.text)
+                  return new Error(e.text);
                 }
                 else {
                   repo.checkout(branch, function (e) {
@@ -42,14 +43,20 @@ var fs = require('fs'),
 
                     repo.delete_branch(seed, function (e) {
 
-                      if (e) { return new Error(e.text); }
-                      else   {console.log('gh-pages updated')}
-                    })
-                  })
+                      if (e) {
+                        return new Error(e.text);
+                      }
+                      else {
+                        grunt.log.oklns(message);
+                      }
+                    });
+                  });
                 }
-              })
-            })
-          })
-        })
-      })
+              });
+            });
+          });
+        });
+      });
     });
+  });
+};
